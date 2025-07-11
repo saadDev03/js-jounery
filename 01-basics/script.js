0dd8f171830af1f7d29a8cace985b42e28b9882c
saadDev03 <saadali.webdev@gmail.com>
2025-07-11 13:21:46 +0000

    configure

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -1,6 +1,35 @@
-let x = "saad" + "" + "ali" ;
- x  += "ali" ;
- console.log(x);
+let  obj1 = {
+    name :  "ali" ,
+    address : "lahore"
+}
+  
+obj2 = obj1
+
+obj2.name = "asghar " 
+
+let x  = 4 ; 
+y = x
+y = 10 
+
+
+
+// primmitive  it store onle one value at a time 
+// it store value directly 
+// each value has its own memory location  
+// it store in stack memeory
+// changing one variable does not effect other
+// it compare value 
+// it copy the direct value 
+
+//non-primitive 
+// it store the reference
+// each variable has one memeory location  
+// it compare reference
+
+
+
+
+
  
 
  
