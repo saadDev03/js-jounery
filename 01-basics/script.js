0d1ebf9bab47ba6bb72530404ba8a11217ee18ae
saadDev03 <saadali.webdev@gmail.com>
2025-07-12 10:06:09 +0000

    configure

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -5,15 +5,21 @@ let  obj1 = {
   
 obj2 = obj1
 
+
+
+
 obj2.name = "asghar " 
 
 let x  = 4 ; 
 y = x
-y = 10 
+y = 3
 
 
 
-// primmitive  it store onle one value at a time 
+
+
+
+// stack memory (primmitive)  it store simple value  
 // it store value directly 
 // Each variable has its own memory location  
 // it store in stack memeory
@@ -21,10 +27,34 @@ y = 10
 // it compare value 
 // it copy the direct value 
 
-//non-primitive 
+// heap memory (non-primitive) 
 // it store the reference
 // Each variable holds a reference to the same memory location.” 
 // it compare reference
+//************************************************************************ */
+// string written  inside `` are tempelate string or template literal (``)  backticks 
+// in backticks there a  string interpolation process and placeholder we can inserted varaible and expression into string  
+// in backticks we can do not use /n for nwe line 
+//++++++++++++++++++++++++++++++++++++++++++++++++//
+// string method A string method is a built-in function that lets you change or check a string.
+//The method does not change the original value.
+//It creates a new value (usually a primitive) and returns it
+//The method does not change the original value.
+//It creates a new value (usually a primitive) and returns it
+// immutable mean Once a value is created, you can’t change it    you can only make a new one .
+ let str1 = "helo world" ;
+ 
+
+ console.log(str1[2]);
+ 
+ 
+   
+
+
+
+
+
+
 
 
 
