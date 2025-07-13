155febf98e9d55195cad1fb4b416f94c641a3972
saadDev03 <saadali.webdev@gmail.com>
2025-07-13 06:27:01 +0000

    configure

--- 01-basics/script.js
+++ 01-basics/script.js
@@ -37,7 +37,7 @@ y = 3
 // in backticks we can do not use /n for nwe line 
 //++++++++++++++++++++++++++++++++++++++++++++++++//
 //string-Method//
-//A method is a function that we use on objects,
+//A method is a function that is stored inside an object and is used to perform an action on that object.
 // string method A string method is a built-in function that lets you change or check a string.
 //A built-in method is a function already provided by JavaScript — you don’t have to create it yourself
 //Because strings are immutable, string methods always return a new string.
@@ -56,7 +56,30 @@ y = 3
 // String Method	              A method built for string values (like .toUpperCase(), .length)
 // Who wraps the string?	       JavaScript automatically wraps the primitive string
 // Why?	                           Because methods only work on objects, and strings are primitives
-// Is the method responsible?	   ❌ No. The method doesn't wrap anything — JavaScript does it internally
+// Is the method responsible?	   ❌ No. The method doesn't wrap anything — JavaScript does it 
+
+//charAt() is a method because it is a function attached to the string object.
+
+//A method is a function that belongs to an object (like a string, array, etc.).
+// So yes — charAt() is a built-in function, but when used on a string like name.charAt(), it becomes a method.l
+// charAt is a function
+
+// It is stored inside String.prototype
+
+// Because it’s inside an object, we call it a method
+
+// Function	         A block of code that does something
+// Method	        A method is a function that is stored inside an object and is used to perform an action on that object.
+// String Method	A built-in function that is stored in the String object
+
+// A string method is just a function stored inside the String object, so we call it a method — b
+// ut it's still a function at its core.
+
+// Feature   	         Function	                                               Method
+// What it is	       A block of code that runs when you call it           	A method is a function that is stored inside an object and is used to perform an action on that object.
+// Stands alone?	    ✅ Yes, works by itself	                              ❌ No, it is part of an object
+// How to call	        Just use the name: greet()	                           Use the object: person.sayHello()
+// Example	            function greet() { }	                                  person.sayHello = function() { }
 
  let str1 = new String("helo world") ;
  
