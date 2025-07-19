96cb839472864d6f3190bc07a62d342e4433eaba
saadDev03 <saadali.webdev@gmail.com>
2025-07-19 07:34:05 +0000

    configure

--- 01-basics/object.js
+++ 01-basics/object.js
@@ -64,3 +64,112 @@
 
 //==>But, there is no need to use new Object()
 //==> For readability, simplicity and execution speed, use the object literal method.
+
+//==>An object method is a function stored as a property in an object.
+//==>A method is a function definition stored as a property value.
+// 
+// const  person = {
+//      name : "ali" ,
+//     adress : "lahore" ,
+//     gmail : "ali@google.com" ,
+// 
+     
+        // object_fun : function name(params) {
+        //     return this.name +   this.gmail ;
+
+            
+        //  }
+     
+
+    //  }
+
+    //  adding function ouside curly bracket
+
+    // person.fun = function ( ){
+    //     return  (this.name + this.gmail) .toUpperCase() ;
+
+    // }
+
+    // console.log(person.name);
+
+    //==> object can be nested
+
+    // const person = {
+//         name : "ali" ,
+//      adress : "lahore" ,
+//     gmail : "ali@google.com" ,
+
+//     product : {
+//         name : "car" ,
+//         color : "white"
+//     }
+
+// }
+//  let outerkey = "product"
+//  let innerkey = "color"
+
+//  console.log(person[outerkey][innerkey]);
+
+
+
+    //+++++++++++++++   Display Object ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\
+    // There are eight way of display object
+    // 1. console.log() 
+    // 2. Access individual properties
+    //  3. Loop through the object (for...in)
+    //  4. Convert object to a string using JSON.stringify()
+    //  5. Show on Webpage (in HTML)
+    //  6. Use Object.entries()
+    //  7. Use Object.values() (only values
+    // 8. Use Object.keys() (only keys)
+
+    //==>1
+
+    const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+console.log(person);
+
+//==> 2
+
+console.log(person.name);
+
+//==> 4
+let result = JSON.stringify(person)
+console.log(result);
+
+
+//==> 5
+// document.getElementById().innerHTML = person ;
+
+//==> 6
+    console.logobO(Object.entries(person));
+
+//==> 7
+console.log(Object.values(person));
+
+   //==> 8
+    console.log(Object.keys(person));
+
+
+    
+
+    
+    
+     
+    
+     
+     
+
+
+
+
+
+
+
+
+     
+
